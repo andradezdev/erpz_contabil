28d49e407223a9856595be23718e62b6dbe8bc86
ERPZ Dev <dev@erpz.io>
2026-09-20 00:39:51 +0000

    fix: Sincroniza relatorio DRE Estruturado e previne duplicidade de desktop icon

--- erpz_contabil/setup.py
+++ erpz_contabil/setup.py
@@ -1,37 +1,44 @@
 import frappe
+import json, os
 
 def setup_contabil_desktop_icon():
-    icon_name = frappe.db.get_value("Desktop Icon", {"parent_icon": "Accounting", "link_to": "ERPZ Contábil"}, "name")
-    if not icon_name:
-        icon_name = frappe.db.get_value("Desktop Icon", {"label": "ERPZ Contábil"}, "name")
-        
-    if icon_name:
-        frappe.db.set_value("Desktop Icon", icon_name, {
-            "label": "ERPZ Contábil",
-            "icon": "calculator",
-            "icon_type": "Link",
-            "link_type": "Workspace Sidebar",
-            "link_to": "ERPZ Contábil",
-            "parent_icon": "Accounting",
-            "hidden": 0,
-            "standard": 1,
-            "app": "erpz_contabil",
-            "idx": 1
-        })
+    icon_name = "ERPZ Contábil"
+    acc_data = {
+        "label": "ERPZ Contábil",
+        "icon": "calculator",
+        "icon_type": "Link",
+        "link_type": "Workspace Sidebar",
+        "link_to": "ERPZ Contábil",
+        "parent_icon": "Accounting",
+        "hidden": 0,
+        "standard": 1,
+        "app": "erpz_contabil",
+        "idx": 1
+    }
+
+    if frappe.db.exists("Desktop Icon", icon_name):
+        frappe.db.set_value("Desktop Icon", icon_name, acc_data)
     else:
         doc = frappe.new_doc("Desktop Icon")
-        doc.name = "ERPZ Contabil Accounting"
-        doc.label = "ERPZ Contábil"
-        doc.icon = "calculator"
-        doc.icon_type = "Link"
-        doc.link_type = "Workspace Sidebar"
-        doc.link_to = "ERPZ Contabil"
-        doc.parent_icon = "Accounting"
-        doc.hidden = 0
-        doc.standard = 1
-        doc.app = "erpz_contabil"
-        doc.idx = 1
+        doc.update(acc_data)
         doc.insert(ignore_permissions=True)
+
+    # Sincroniza Workspace Sidebar
+    sb_file = "/home/frappe/frappe-bench/apps/erpz_contabil/erpz_contabil/workspace_sidebar/erpz_contabil.json"
+    if os.path.exists(sb_file):
+        with open(sb_file, "r", encoding="utf-8") as fp:
+            sb_data = json.load(fp)
+
+        if frappe.db.exists("Workspace Sidebar", "ERPZ Contábil"):
+            sb = frappe.get_doc("Workspace Sidebar", "ERPZ Contábil")
+            sb.items = []
+            for it in sb_data.get("items", []):
+                sb.append("items", it)
+            sb.save(ignore_permissions=True)
+        else:
+            sb = frappe.new_doc("Workspace Sidebar")
+            sb.update(sb_data)
+            sb.insert(ignore_permissions=True)
 
 def after_install():
     setup_contabil_desktop_icon()
